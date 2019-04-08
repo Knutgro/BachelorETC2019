0c74601c6a40e00583610476d7e9827ab1942570
Sindreoestrem <sindrost@stud.ntnu.no>
2019-04-08 12:22:36 +0200

    Changed search filter and search results to show listings instead of vehicles. This time with the search bar

--- trader/src/main/java/com/etc/trader/security/WebSecurityConfig.java
+++ trader/src/main/java/com/etc/trader/security/WebSecurityConfig.java
@@ -67,6 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/listings/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
