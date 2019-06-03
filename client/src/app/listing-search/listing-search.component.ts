f0bb8400a8fd4b29a69d5c933d81a115b1d9e0a1
knutgro <knutgro@stud.ntnu.no>
2019-06-03 13:17:04 +0200

    listingsearch

---
+++ client/src/app/listing-search/listing-search.component.ts
@@ -0,0 +1,59 @@
+import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Listing} from '../_models/listing';
+import {ListingService} from '../_services/listing.service';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {AlertService} from '../_services/alert.service';
+import {first} from 'rxjs/operators';
+import {ImagesService} from '../_services/images.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-listing-search',
+  templateUrl: './listing-search.component.html',
+  styleUrls: ['./listing-search.component.scss']
+})
+export class ListingSearchComponent implements OnInit, OnDestroy {
+  listings: Listing[];
+  dataListings: MatTableDataSource<Listing>;
+  obs: Observable<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(    private listingsService: ListingService,
+                  private alertService: AlertService,
+                  private imagesService: ImagesService,
+                  private changeDetectorRef: ChangeDetectorRef) {
+    this.listingsService = listingsService;
+    this.alertService = alertService;
+    this.imagesService = imagesService;
+    this.changeDetectorRef = changeDetectorRef;
+  }
+
+  ngOnInit() {
+    this.loadAllListings();
+  }
+
+  ngOnDestroy() {
+    if (this.dataListings) {
+      this.dataListings.disconnect();
+    }
+  }
+
+  private loadAllListings() {
+    this.listingsService.getAll().pipe(first()).subscribe(listings => {
+      this.listings = listings;
+      this.dataListings = new MatTableDataSource(listings);
+      this.changeDetectorRef.detectChanges();
+      this.dataListings.paginator = this.paginator;
+      this.obs = this.dataListings.connect();
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataListings.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataListings.paginator) {
+      this.dataListings.paginator.firstPage();
+    }
+  }
+
+}
